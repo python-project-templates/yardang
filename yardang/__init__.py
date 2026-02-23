5e6490ed1d16393a695b5407aefa93db1d6de907
Tim Paine <3105306+timkpaine@users.noreply.github.com>
2026-02-23 14:36:18 -0500

    Bump version: 0.4.1 → 0.4.2
    Signed-off-by: Tim Paine <3105306+timkpaine@users.noreply.github.com>

--- yardang/__init__.py
+++ yardang/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "0.4.1"
+__version__ = "0.4.2"
 
 from .build import generate_docs_configuration, generate_wiki_configuration, run_doxygen_if_needed
 from .wiki import process_wiki_output
