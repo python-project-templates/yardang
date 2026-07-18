d978263bdba5241e7666c534429514b54cf0a712
Tim Paine <3105306+timkpaine@users.noreply.github.com>
2026-07-18 13:24:56 -0400

    Bump version: 0.6.0 → 0.6.1
    Signed-off-by: Tim Paine <3105306+timkpaine@users.noreply.github.com>

--- yardang/__init__.py
+++ yardang/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "0.6.0"
+__version__ = "0.6.1"
 
 from .build import BUNDLED_THEMES, generate_docs_configuration, generate_wiki_configuration, run_doxygen_if_needed
 from .wiki import process_wiki_output
